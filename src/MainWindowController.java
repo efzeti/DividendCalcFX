31956367d46ba05dd6f2e32ae96cc650ef35fe6a
efzeti <00franio@gmail.com>
2021-01-30 20:25:33 +0100

    .fxml now roughly shows the final appearance of program. MainWindowController has all the required fields.

--- src/MainWindowController.java
+++ src/MainWindowController.java
@@ -1,3 +1,13 @@
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 public class MainWindowController {
+    public TextField stockPrice;
+    public TextField quarterlyDividend;
+    public TextField monthlyInvestments;
+    public TextField initialStockAmount;
+    public DatePicker deadLine;
+    public TextField finalStatus;
+
+
 }
